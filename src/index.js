04d2af5353eada28c46c0e0cf1de2e1908be46c9
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-04-22 18:26:37 -0700

    trying to get deployed app to access camera

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ root.render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
