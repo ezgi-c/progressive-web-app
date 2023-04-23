0a8710030b3cdf9aba8639cce8aeebd05480bf71
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-04-22 19:46:10 -0700

    revert to previous commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const filesToCache = [
   "/camera-icon(s).png",
 ];
 
-this.addEventListener("install", (e) => {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(filesToCache);
@@ -15,7 +16,8 @@ this.addEventListener("install", (e) => {
   );
 });
 
-this.addEventListener("fetch", (e) => {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
       return response || fetch(e.request);
