9529583f81d173d132957ff5b3f57c51480ae014
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-04-25 01:39:01 -0700

    added timer

--- src/Components/CameraFeed.js
+++ src/Components/CameraFeed.js
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useTakePicture from "../hooks/useTakePicture";
 
 function CameraFeed({ setFlash }) {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
+  const [countdown, setCountdown] = useState(null);
 
   const { takePicture } = useTakePicture(videoRef, canvasRef, setFlash);
 
@@ -21,17 +22,33 @@ function CameraFeed({ setFlash }) {
     // eslint-disable-next-line
   }, []);
 
+  const startCountdown = () => {
+    setCountdown(3);
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(timer);
+      setCountdown(null);
+      takePicture();
+    }, 3000);
+  };
+
   return (
     <div className="video-container">
       <video ref={videoRef} autoPlay muted className="video"></video>
       <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
-      <button
-        className="take-picture"
-        title="Click to take a picture!"
-        onClick={takePicture}
-      >
-        Take a Picture 📸
-      </button>
+      {countdown ? (
+        <div className="countdown">{countdown}</div>
+      ) : (
+        <button
+          className="take-picture"
+          title="Click to take a picture!"
+          onClick={startCountdown}
+        >
+          Take a Picture 📸
+        </button>
+      )}
     </div>
   );
 }
